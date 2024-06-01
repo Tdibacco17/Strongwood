6ca7b7b6ae9432f94102c08add083226a29411f0
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2024-06-01 20:29:41 -0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    deviceSizes: [6048],
     // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     images: {
+        deviceSizes: [6048],
         minimumCacheTTL: 315360000,
         formats: ['image/avif', 'image/webp'],
     },
