058201ff70762a72b35d30c34466a810c28e07bb
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2024-08-19 21:57:25 -0300

    meta pixel 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import type { Viewport } from 'next'
 import "./globals.scss";
 import { roboto } from "@/utils/fonts";
-import { Suspense } from "react";
-import { FacebookPixelEvents } from "@/components/MetaPixelComponent/MetaPixelComponent";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "StrongWood®",
@@ -32,13 +31,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '308659638359795');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=308659638359795&ev=PageView&noscript=1"
+        />
+      </noscript>
       <body className={roboto.className}>
         <main id="top">
           {children}
         </main>
-        <Suspense fallback={null}>
-          <FacebookPixelEvents />
-        </Suspense>
       </body>
     </html>
   );
